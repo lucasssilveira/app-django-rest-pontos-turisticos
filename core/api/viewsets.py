5e47296bc8ffbf86421d990de7b0f948e765c9a3
lucas <lukas.silveira@hotmail.com>
2020-01-16 20:45:30 -0300

    Inicio da utilização da API , pontos turisticos

---
+++ core/api/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+from core.models import PontoTuristico
+from .serializers import PontoTuristicoSerializer
+
+
+class  PontoTuristicoViewSet(ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    queryset = PontoTuristico.objects.all()
+    serializer_class = PontoTuristicoSerializer
