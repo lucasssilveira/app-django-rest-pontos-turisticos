e506c8b7b2f7902470f727b8fdb62a49b4239bf6
lucas <lukas.silveira@hotmail.com>
2020-01-18 16:04:39 -0300

    modificando metodos padrões

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -10,5 +11,21 @@ class  PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
 
+
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    #def list(self, request, *args, **kwargs):
+   #     return Response({'teste': 123})
+
+    #def create(self, request, *args, **kwargs):
+      #  return Response({'Hello': request.data['nome']})
+
+      #def destroy(self, request, *args, **kwargs):
+
+    #  def retrieve(self, request, *args, **kwargs): tipo o get mas para no item
+
+    # def update(self, request, *args, **kwargs): método put
+
+    # def partial_update(self, request, *args, **kwargs): metodo path
+
